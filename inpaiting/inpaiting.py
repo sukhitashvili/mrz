bccbb14b30f97f1b521f43d6260c833b75747625
Soso-Sukhitashvili <sukhitashvili.soso@gmail.com>
2020-06-03 17:04:56 +0400

    added coordinates for function

--- inpaiting/inpaiting.py
+++ inpaiting/inpaiting.py
@@ -1,11 +1,20 @@
 import cv2
 import numpy as np
 import os
+from typing import Tuple
 
 kernel = np.ones((5, 5), np.uint8)
 
 
-def inpaint(bgr_image, mask_h_per_from_bottom=0.35, radius: int = 10):
+def inpaint(bgr_image, x_y: Tuple = (50, 200), height_width: Tuple = (100, 300), radius: int = 10):
+    '''
+    remove black color text form specified area.
+    :param bgr_image: opencv-python image (BGR);
+    :param x_y: x,y coordinates of top-left part of the text;
+    :param height_width: height and width of the text;
+    :param radius: the parameter for inpainting function;
+    :return: the same size and format image with removed text.
+    '''
     org_width = bgr_image.shape[1]
     org_height = bgr_image.shape[0]
     width = 600
@@ -14,15 +23,14 @@ def inpaint(bgr_image, mask_h_per_from_bottom=0.35, radius: int = 10):
     bgr_image = cv2.resize(bgr_image, dim, interpolation=cv2.INTER_AREA)
 
     flags = cv2.INPAINT_TELEA
-    mask_top = np.zeros([int(np.round(bgr_image.shape[0] * (1 - mask_h_per_from_bottom))), bgr_image.shape[1]],
-                        dtype=np.uint8)
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
     mask = (gray_image < 140).astype('uint8')
+    zero_mask = np.zeros(mask.shape, dtype=np.uint8)
+    zero_mask[x_y[1]:(x_y[1]+height_width[0]), x_y[0]:(x_y[0]+height_width[1])] = 1
+    mask = mask * zero_mask
 
-    mask_bottom = mask[int(np.round(bgr_image.shape[0] * (1 - mask_h_per_from_bottom))):, ...]
-    mask_bottom = cv2.dilate(mask_bottom, kernel, iterations=1)
-    mask_bottom = cv2.medianBlur(mask_bottom, 7)
-    mask = np.vstack((mask_top, mask_bottom))
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    mask = cv2.medianBlur(mask, 7)
     output = cv2.inpaint(bgr_image, mask, radius, flags=flags)
     dim = (org_width, org_height)
     output = cv2.resize(output, dim, interpolation=cv2.INTER_AREA)
@@ -36,6 +44,8 @@ if __name__ == '__main__':
     for name in ld:
         read_path = os.path.join(path_1, name)
         image = cv2.imread(read_path)
-        output = inpaint(image)
+        output = inpaint(image, )
         save_path = os.path.join(path_2, name)
-        cv2.imwrite(save_path, output)
+        # cv2.imwrite(save_path, output)
+        cv2.imshow(name, output)
+        cv2.waitKeyEx(0)
